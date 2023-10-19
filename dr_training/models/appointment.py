85d04af035319c163b2da1a0f6c03e3b2cf5d71a
mertcaliskanlinux <mertcaliskanlinux@gmail.com>
2023-10-19 10:42:35 +0300

    r

--- dr_training/models/appointment.py
+++ dr_training/models/appointment.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, exceptions
-from .sale import  SaleOrder
 
 
 
@@ -20,38 +19,111 @@ class Appointment(models.Model):
     doctor_full_name = fields.Char(string="Doctor Name", compute="_compute_doctor_full_name", store=True)
     is_readonly = fields.Boolean(string="Is Readonly", compute="_compute_is_readonly")
     total_amount = fields.Float(string="Total Amount", compute="_compute_total_amount", store=True)
-    sale_order_count = fields.Integer(string="Sale Orders", compute="_compute_sale_order_count")
-    sale_order_id = fields.Many2one(comodel_name="sale.order", string="Sale Order")
     appointment_id = fields.Many2one('dr_patients.appointment', string='Appointment')
+    
+    
     invoice_ids = fields.One2many('account.move', 'appointment_id', string='Invoice', compute='_compute_invoice_ids')
     invoice_count = fields.Integer(string='Invoice Count', compute='_compute_invoice_count')
+    account_move_id = fields.Many2one('account.move', string='Invoice')
+
+
+    sale_order_line_ids = fields.One2many('sale.order.line', 'appointment_id', string="Sale Order Line")
+    sale_order_count = fields.Integer(string='Sale Orders', compute='_compute_sale_order_count')
+    sale_order_ids = fields.One2many('sale.order', 'appointment_id', string="Sales Order")
+    sale_order_id = fields.Many2one('sale.order')
+
+    partner_id = fields.Many2one('res.partner', "Patient Partner") #bu partner_id sale order da kullanılıyor.
+
+
+
+
+    def action_view_invoice(self):
+        self.ensure_one()  
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Invoices',
+            'res_model': 'account.move',
+            'view_mode': 'tree,form',
+            'domain': [('appointment_id', '=', self.id)],  # Filter by appointment
+            'context': {'default_appointment_id': self.id},  # Set the default appointment
+            'stage': 'posted',
+        }
+    
 
 
     @api.depends('invoice_ids')
     def _compute_invoice_ids(self):
-        for appointment in self:
-            appointment.invoice_ids = self.env['account.move'].search([('appointment_id', '=', appointment.id)])
+        for rec in self:
+            rec.invoice_ids = self.env['account.move'].search([('appointment_id', '=', rec.id)])
+
 
     @api.depends('invoice_count')
     def _compute_invoice_count(self):
         for appointment in self:
             appointment.invoice_count = len(appointment.invoice_ids)
 
-    def action_view_invoices(self):
-
-        invoices = self.invoice_ids
-        print(f"invoices: {invoices}")
 
 
+    # def action_create_sale_order(self):
+    #     self.ensure_one()
+    #     if self.patient:
+    #         partner = self.env['res.partner'].create({
+    #             'name': self.patient.first_name + " " + self.patient.last_name,
+    #             'phone': self.patient.phone,
+    #             'email': self.patient.email,
+    #         })
+
+    #         self.partner_id = partner
+
+    #     sale_order = self.env['sale.order'].create({
+    #         'appointment_id': self.id,
+    #         'partner_id': self.partner_id.id,
+    #     })
+
+    #     sale_order_line = self.env['sale.order.line'].create({
+    #         'order_id': sale_order.id,
+    #         'product_id': self.env['product.product'].search([('name', '=', 'Injections')]).id,
+    #     })
+
+    #     return {
+    #         'type': 'ir.actions.act_window',
+    #         'name': 'Sale Order',
+    #         'res_model': 'sale.order',
+    #         'res_id': sale_order.id,
+    #         'view_mode': 'form',
+    #         'target': 'current',
+    #     }
+    
+    def create_sale_order(self):
+
+        self.ensure_one()
+        if self.patient:
+            self.partner_id = self.env['res.partner'].create({
+                'name': self.patient.first_name + " " + self.patient.last_name,
+                'phone': self.patient.phone,
+                'email': self.patient.email,
+            })
+        sale_order = self.env['sale.order'].create({
+            'appointment_id': self.id,
+            'partner_id': self.partner_id.id,
+
+        })
+
+        # sale_order_line = self.env['sale.order.line'].create({
+        #     'order_id': sale_order.id,
+        #     'product_id': self.env['product.product'].search([('name', '=', 'Injections')]).id,
+        # })
 
         return {
-            'name': 'Invoices',
-            'view_mode': 'tree,form',
-            'res_model': 'account.move',
-            'domain': [('id', 'in', invoices.ids)],
             'type': 'ir.actions.act_window',
+            'name': 'Sale Order',
+            'res_model': 'sale.order',
+            'res_id': sale_order.id,
+            'view_mode': 'form',
+            'target': 'current',
         }
-
+    
+    
     @api.depends('patient')
     def _compute_patient_full_name(self):
         for appointment in self:
@@ -63,12 +135,40 @@ class Appointment(models.Model):
             appointment.doctor_full_name = ', '.join(
                 appointment.doctor_id.mapped('full_name')) if appointment.doctor_id else ""
 
-    @api.depends('appointment_id')
+    @api.depends('sale_order_line_ids')
     def _compute_sale_order_count(self):
-        for appointment in self:
-            sale_order_count = self.env['sale.order'].search_count([('appointment_id', '=', appointment.id)])
-            appointment.sale_order_count = sale_order_count
+        for rec in self:
+            sale_order_count = self.env['sale.order'].search_count([('appointment_id', '=', self.id)])
+            rec.sale_order_count = sale_order_count
 
+
+    @api.model
+    def create(self, vals):
+        if vals.get('code', 'New') == 'New':
+            vals['code'] = self.env['ir.sequence'].next_by_code('dr_patients.appointment') or 'New'
+        return super(Appointment, self).create(vals)
+
+    @api.constrains('code')
+    def _check_code_unique(self):
+        for record in self:
+            if self.env['dr_patients.appointment'].search_count([('code', '=', record.code)]) > 1:
+                raise exceptions.ValidationError('The Code must be unique.')
+
+
+    def action_sale_order(self):
+        self.ensure_one()  # Ensure you're working with a single record.
+        action = {
+            'type': 'ir.actions.act_window',
+            'name': 'Sale Orders',
+            'res_model': 'sale.order',
+            'view_mode': 'tree,form',
+            'domain': [('appointment_id', '=', self.id)],  # Filter by appointment
+            'context': {'default_appointment_id': self.id},  # Set the default appointment
+        }
+        return action
+    
+
+    
     def action_in_progress(self):
         self.write({'stage': 'in_progress'})
 
@@ -85,39 +185,3 @@ class Appointment(models.Model):
         if self.filtered(lambda appointment: appointment.stage == 'done'):
             raise exceptions.ValidationError("You cannot delete a done appointment")
         return super(Appointment, self).unlink()
-
-
-    @api.model
-    def create(self, vals):
-        if vals.get('code', 'New') == 'New':
-            vals['code'] = self.env['ir.sequence'].next_by_code('dr_patients.appointment') or 'New'
-        return super(Appointment, self).create(vals)
-
-    @api.constrains('code')
-    def _check_code_unique(self):
-        for record in self:
-            if self.env['dr_patients.appointment'].search_count([('code', '=', record.code)]) > 1:
-                raise exceptions.ValidationError('The Code must be unique.')
-
-    def action_sale_order(self):
-        self.ensure_one()  # Ensure that it works for a single record.
-
-        # Create a new Sale Order record in the 'draft' state
-        created_sale_order = self.env['sale.order'].create({
-            'partner_id': self.patient.id,
-            'appointment_id': self.id,  # Link the Sale Order to the current appointment
-        })
-
-        # Open the created Sale Order for further editing
-        action = {
-            'type': 'ir.actions.act_window',
-            'name': 'Sale Orders',
-            'res_model': 'sale.order',
-            'view_mode': 'form',
-            'res_id': created_sale_order.id,
-            'context': {
-                'default_appointment_id': self.id,  # Set the appointment for reference
-            },
-        }
-
-        return action
--- dr_training/models/sale.py
+++ dr_training/models/sale.py
@@ -1,31 +1,30 @@
-# -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
+
 class SaleOrder(models.Model):
-
     _inherit = 'sale.order'
-
     appointment_id = fields.Many2one('dr_patients.appointment', string='Appointment')
-    appointment_count = fields.Integer(string='Appointment', compute='_compute_appointment_count')
-    invoice_ids = fields.One2many('account.move', 'appointment_id', string='Invoice', compute='_compute_invoice_ids')
-    invoice_count = fields.Integer(string='Invoice Count', compute='_compute_invoice_count')
+    appointment_count = fields.Integer(string='Appointment', compute='_compute_appointment_count', store=True)
+    invoice_count = fields.Integer(string='Invoice', compute='_compute_invoice_count')
+    invoice_ids = fields.One2many('account.move', 'appointment_id', string='Invoice', compute="_compute_invoice_ids", store="True")
 
-    @api.depends('invoice_ids', 'appointment_id')
-    def _compute_appointment_count(self):
-        for order in self:
-            order.appointment_count = len(order.appointment_id) if order.appointment_id else 0
 
     @api.depends('appointment_id')
-    def _compute_invoice_ids(self):
-        for appointment in self:
-            appointment.invoice_ids = self.env['account.move'].search([('appointment_id', '=', appointment.id)])
+    def _compute_appointment_count(self):
+        for rec in self:
+            rec.appointment_count = len(rec.appointment_id) if rec.appointment_id else 0
 
     @api.depends('invoice_ids')
+    def _compute_invoice_ids(self):
+        for rec in self:
+            rec.invoice_ids = self.env['account.move'].search([('appointment_id', '=', rec.id)])
+
+    @api.depends('invoice_ids', 'appointment_id')
     def _compute_invoice_count(self):
-        for appointment in self:
-            appointment.invoice_count = len(appointment.invoice_ids)
+        for rec in self:
+            rec.invoice_count = len(rec.invoice_ids)
 
-    def action_view_sale_appointment(self):
+    def action_open_appointments(self):
         self.ensure_one()
         return {
             'type': 'ir.actions.act_window',
@@ -33,42 +32,39 @@ class SaleOrder(models.Model):
             'view_mode': 'form',
             'res_model': 'dr_patients.appointment',
             'res_id': self.appointment_id.id,
-            'context': {'create': False},
+            'context': {'create': True, 'edit': False},
             'target': 'current',
         }
 
-    def action_view_appointment(self):
-        self.ensure_one()
-        return {
-            'type': 'ir.actions.act_window',
-            'name': 'Appointment',
-            'view_mode': 'form',
-            'res_model': 'dr_patients.appointment',
-            'res_id': self.appointment_id.id,
-            'context': {'create': True},
-            'target': 'current',
-        }
-
-    def action_invoice(self):
+    def action_open_invoice(self):
         self.ensure_one()
 
-        # Create a new invoice
-        invoice = self.env['account.move'].create({
-            'appointment_id': self.appointment_id.id,  # Set the appointment on the invoice
-            # Other fields for the invoice, such as partner_id, product lines, etc.
+        invoice_id = self.env['account.move'].create({
+            'appointment_id': self.appointment_id.id,
+            'ref': self.name,
         })
 
-        # Optionally, you can manually post (confirm) the invoice
-        invoice.post()
 
         return {
             'type': 'ir.actions.act_window',
             'name': 'Invoice',
             'res_model': 'account.move',
+            'res_id': invoice_id.id,
+            'context': {'create': False, 'edit': False},
             'view_mode': 'form',
-            'res_id': invoice.id,
-            'context': {'create': False},
             'target': 'current',
+        }
+
+    def action_view_invoice(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Invoices',
+            'res_model': 'account.move',
+            'view_mode': 'tree,form',
+            'domain': [('appointment_id', '=', self.appointment_id.id)], 
+            'context': {'default_appointment_id': self.appointment_id.id},  
+            'stage': 'posted',
         }
 
 
